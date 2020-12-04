4d9d0498fdb095735422dae8624873a838d7931d
Rubel Hassan <rubel.hassan@dsinnovators.com>
2020-12-04 22:25:23 +0600

    Initialize project skeleton

---
+++ src/main/java/com/dsinnovators/devprofilesbackend/DevProfilesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dsinnovators.devprofilesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProfilesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProfilesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsinnovators/devprofilesbackend/DevProfilesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsinnovators.devprofilesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProfilesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
