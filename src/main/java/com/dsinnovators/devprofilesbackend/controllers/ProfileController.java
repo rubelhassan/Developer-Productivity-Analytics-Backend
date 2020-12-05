dca4e377158d4248a9216aaeae03b9105a84e23e
Rubel Hassan <rubel.hassan@dsinnovators.com>
2020-12-06 03:57:43 +0600

    Add repositories and other fields to profile query

--- src/main/java/com/dsinnovators/devprofilesbackend/controllers/ProfileController.java
+++ src/main/java/com/dsinnovators/devprofilesbackend/controllers/ProfileController.java
@@ -2,17 +2,24 @@ package com.dsinnovators.devprofilesbackend.controllers;
 
 import com.dsinnovators.devprofilesbackend.github.GithubGraphqlClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
+@RequestMapping(value="profiles", produces = "application/json")
+@AllArgsConstructor
 public class ProfileController {
-    @Autowired
     private GithubGraphqlClient githubGraphqlClient;
 
-    @GetMapping("/profiles")
-    String getUserProfile() throws JsonProcessingException {
-        return githubGraphqlClient.fetchUserProfileWithContributions("97dcb67c1aef75929b6e8ffb9c7de47f627f7962");
+    @GetMapping
+    ResponseEntity<String> getUserProfile() throws JsonProcessingException {
+        // TODO: Need to refactor with stored token and parameterized profile id
+        String token = "place test token here";
+        return ok(githubGraphqlClient.fetchUserProfileWithContributions(token));
     }
 }
