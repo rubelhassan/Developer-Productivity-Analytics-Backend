ac04069c414aa2fd54b22cda4c21d030f166cb16
Rubel Hassan <rubel.hassan@dsinnovators.com>
2020-12-09 10:10:55 +0600

    Refactor security configurer

--- src/main/java/com/dsinnovators/devprofilesbackend/configurations/ResourceServerConfig.java
+++ src/main/java/com/dsinnovators/devprofilesbackend/configurations/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.dsinnovators.devprofilesbackend.configurations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
@@ -13,12 +14,9 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().mvcMatchers("/**").permitAll();
 
         http
-            .mvcMatcher("/**")
-                .authorizeRequests()
-                .mvcMatchers("/profiles/**").access("hasAuthority('SCOPE_api.read')")
-                .mvcMatchers("/**").access("hasAuthority('SCOPE_api.write')")
-            .and()
-            .oauth2ResourceServer()
-                .jwt();
+            .authorizeRequests(configurer -> configurer
+                    .mvcMatchers("/profiles/**").access("hasAuthority('SCOPE_api.read')")
+                    .mvcMatchers("/**").access("hasAuthority('SCOPE_api.write')"))
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
